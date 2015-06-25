028b206595ee55f15692020b43712f6a793b2802
Vaidotas Valuckas <vaidotas.valuckas@klarna.com>
2015-06-25 09:52:29 +0200

    adding tmux config

--- install-config.sh
+++ install-config.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-rm ~/.vimrc
+rm -f ~/.vimrc
 ln -s ${PWD}/vim/.vimrc ~/.vimrc
 rm -rf ~/.vim
 ln -s ${PWD}/vim/.vim ~/.vim
+
+rm -f ~/.tmux.conf
+ln -s ${PWD}/tmux/.tmux.conf ~/.tmux.conf
+rm -rf ~/.tmux
+ln -s ${PWD}/tmux/.tmux ~/.tmux
