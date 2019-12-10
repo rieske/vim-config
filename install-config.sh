5b91210bb38418ce2b380a5e818d048f984ca176
Vaidotas Valuckas <vvaluckas@gradle.com>
2019-12-10 21:51:29 +0200

    Update tools install script to clone Vundle

--- install-config.sh
+++ install-config.sh
@@ -4,6 +4,7 @@ rm -f ~/.vimrc
 ln -s ${PWD}/vim/.vimrc ~/.vimrc
 rm -rf ~/.vim
 ln -s ${PWD}/vim/.vim ~/.vim
+git clone git@github.com:VundleVim/Vundle.vim.git vim/.vim/bundle/Vundle.vim/
 
 rm -f ~/.tmux.conf
 ln -s ${PWD}/tmux/.tmux.conf ~/.tmux.conf
